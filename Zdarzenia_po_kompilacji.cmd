301d2466430221b1c11da6507472ec43b2a49b42
Revok <revok7@gmail.com>
2022-09-19 03:15:51 +0200

    v.1.81

--- Zdarzenia_po_kompilacji.cmd
+++ Zdarzenia_po_kompilacji.cmd
@@ -2,25 +2,25 @@
 chcp 65001
 
 ::Debug
-xcopy "C:\Users\revok\Documents\Visual Studio 2022\Revok-projekty\C#\PWRlangConverter\!!!plik_konfiguracyjny_autokopiowany_po_kompilacji.json" "C:\Users\revok\Documents\Visual Studio 2022\Revok-projekty\C#\PWRlangConverter\PWRlangConverter\bin\Debug\net6.0" /Y
-if exist "C:\Users\revok\Documents\Visual Studio 2022\Revok-projekty\C#\PWRlangConverter\PWRlangConverter\bin\Debug\net6.0\!!!plik_konfiguracyjny_autokopiowany_po_kompilacji.json" (
-   move /Y "C:\Users\revok\Documents\Visual Studio 2022\Revok-projekty\C#\PWRlangConverter\PWRlangConverter\bin\Debug\net6.0\!!!plik_konfiguracyjny_autokopiowany_po_kompilacji.json" "C:\Users\revok\Documents\Visual Studio 2022\Revok-projekty\C#\PWRlangConverter\PWRlangConverter\bin\Debug\net6.0\cfg.json"
+xcopy "!!!plik_konfiguracyjny_autokopiowany_po_kompilacji.json" "PWRlangConverter\bin\Debug\net6.0" /Y
+if exist "PWRlangConverter\bin\Debug\net6.0\!!!plik_konfiguracyjny_autokopiowany_po_kompilacji.json" (
+   move /Y "PWRlangConverter\bin\Debug\net6.0\!!!plik_konfiguracyjny_autokopiowany_po_kompilacji.json" "PWRlangConverter\bin\Debug\net6.0\cfg.json"
 )
-if exist "C:\Users\revok\Documents\Visual Studio 2022\Revok-projekty\C#\PWRlangConverter\kompatybilność z win7-x64\api-ms-win-core-winrt-l1-1-0.dll" (
-  xcopy "C:\Users\revok\Documents\Visual Studio 2022\Revok-projekty\C#\PWRlangConverter\kompatybilność z win7-x64\api-ms-win-core-winrt-l1-1-0.dll" "C:\Users\revok\Documents\Visual Studio 2022\Revok-projekty\C#\PWRlangConverter\PWRlangConverter\bin\Debug\net6.0\" /Y
+if exist "kompatybilność z win7-x64\api-ms-win-core-winrt-l1-1-0.dll" (
+  xcopy "kompatybilność z win7-x64\api-ms-win-core-winrt-l1-1-0.dll" "PWRlangConverter\bin\Debug\net6.0\" /Y
 )
 
 ::Release_publish
-xcopy "C:\Users\revok\Documents\Visual Studio 2022\Revok-projekty\C#\PWRlangConverter\!!!plik_konfiguracyjny_autokopiowany_po_kompilacji.json" "C:\Users\revok\Documents\Visual Studio 2022\Revok-projekty\C#\PWRlangConverter\PWRlangConverter\bin\Release_publish\net6.0\PWRlangConverter_e1ps_winx64\" /Y
-if exist "C:\Users\revok\Documents\Visual Studio 2022\Revok-projekty\C#\PWRlangConverter\PWRlangConverter\bin\Release_publish\net6.0\PWRlangConverter_e1ps_winx64\!!!plik_konfiguracyjny_autokopiowany_po_kompilacji.json" (
-   move /Y "C:\Users\revok\Documents\Visual Studio 2022\Revok-projekty\C#\PWRlangConverter\PWRlangConverter\bin\Release_publish\net6.0\PWRlangConverter_e1ps_winx64\!!!plik_konfiguracyjny_autokopiowany_po_kompilacji.json" "C:\Users\revok\Documents\Visual Studio 2022\Revok-projekty\C#\PWRlangConverter\PWRlangConverter\bin\Release_publish\net6.0\PWRlangConverter_e1ps_winx64\cfg.json"
+xcopy "!!!plik_konfiguracyjny_autokopiowany_po_kompilacji.json" "PWRlangConverter\bin\Release_publish\net6.0\PWRlangConverter_e1ps_winx64\" /Y
+if exist "PWRlangConverter\bin\Release_publish\net6.0\PWRlangConverter_e1ps_winx64\!!!plik_konfiguracyjny_autokopiowany_po_kompilacji.json" (
+   move /Y "PWRlangConverter\bin\Release_publish\net6.0\PWRlangConverter_e1ps_winx64\!!!plik_konfiguracyjny_autokopiowany_po_kompilacji.json" "PWRlangConverter\bin\Release_publish\net6.0\PWRlangConverter_e1ps_winx64\cfg.json"
 )
-if exist "C:\Users\revok\Documents\Visual Studio 2022\Revok-projekty\C#\PWRlangConverter\kompatybilność z win7-x64\api-ms-win-core-winrt-l1-1-0.dll" (
-  xcopy "C:\Users\revok\Documents\Visual Studio 2022\Revok-projekty\C#\PWRlangConverter\kompatybilność z win7-x64\api-ms-win-core-winrt-l1-1-0.dll" "C:\Users\revok\Documents\Visual Studio 2022\Revok-projekty\C#\PWRlangConverter\PWRlangConverter\bin\Release_publish\net6.0\PWRlangConverter_e1ps_winx64\" /Y
+if exist "kompatybilność z win7-x64\api-ms-win-core-winrt-l1-1-0.dll" (
+  xcopy "kompatybilność z win7-x64\api-ms-win-core-winrt-l1-1-0.dll" "PWRlangConverter\bin\Release_publish\net6.0\PWRlangConverter_e1ps_winx64\" /Y
 )
-if exist "C:\Users\revok\Documents\Visual Studio 2022\Revok-projekty\C#\PWRlangConverter\!INSTRUKCJA_IMPLEMENTACJI.txt" (
-  xcopy "C:\Users\revok\Documents\Visual Studio 2022\Revok-projekty\C#\PWRlangConverter\!INSTRUKCJA_IMPLEMENTACJI.txt" "C:\Users\revok\Documents\Visual Studio 2022\Revok-projekty\C#\PWRlangConverter\PWRlangConverter\bin\Release_publish\net6.0\PWRlangConverter_e1ps_winx64\" /Y
-)
+::if exist "!INSTRUKCJA_IMPLEMENTACJI.txt" (
+::  xcopy "!INSTRUKCJA_IMPLEMENTACJI.txt" "PWRlangConverter\bin\Release_publish\net6.0\PWRlangConverter_e1ps_winx64\" /Y
+::)
 
 
 
